0418701a3097d1f8e054ccb1ca492b0cd9bf06d3
Diana Catalina Olarte <diana.olarte@edunext.co>
2023-01-06 20:07:15 +1100

    chore: add style dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jxs,tsx}',
+    './components/**/*.{js,ts,jxs,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
